3cfb6ee671b2ef2f0e26caedeb51efec681c52b4
HiSooKa0 <62574751+HiSooKa0@users.noreply.github.com>
2021-04-28 12:45:00 +0000

    Add initial ignite files

---
+++ index.js
@@ -0,0 +1,20 @@
+
+  var x = document.getElementById("Login_Form") ;
+  var y = document.getElementById("Register_Form");
+  var z = document.getElementById("btn");
+  function Register(){
+    x.style.left = "-400px";
+    y.style.left = "60px";
+    z.style.left = "113px";
+
+  }
+  function Login(){
+    x.style.left = "60px";
+    y.style.left = "460px";
+    z.style.left = "0";
+  }
+/*var a = document.getElementById("coll");
+
+function Recover(){
+  a.style.marginTop = "-50px";
+}*/
